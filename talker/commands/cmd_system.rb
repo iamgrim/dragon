cc9874668de25edd3edeb60c14c3191544728a5d
TheBear <james@forluna.co.uk>
2010-02-28 13:58:38 +0000

    Su command

--- talker/commands/cmd_system.rb
+++ talker/commands/cmd_system.rb
@@ -195,4 +195,17 @@ module Commands
     output title_line("Your Private History") + "\n" + history.to_s(get_timestamp_format, get_timezone) + "\n" + blank_line
   end
   define_alias 'myhistory', 'rhistory'
+  
+  define_command 'su' do |message|
+    if rank > 0
+      if message.blank?
+        output "Format: su <message>"
+      else
+        c = Commands.lookup('tell')
+        c.execute(self, "#{name} #{message}") if c
+      end
+    else
+      output "You are not prestigious enough to use that."
+    end
+  end
 end
