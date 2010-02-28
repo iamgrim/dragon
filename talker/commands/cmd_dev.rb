7c3acca19cf4389cb87140e3f2c6e54a5657b5de
TheBear <james@forluna.co.uk>
2010-02-28 10:41:22 +0000

    Remove fix after use

--- talker/commands/cmd_dev.rb
+++ talker/commands/cmd_dev.rb
@@ -49,15 +49,7 @@ module Commands
       output "No permission."
     end
   end
-  
-  define_command 'fine_sock', :invisible => true do
-    if developer?
-      u = find_user("sockeye")
-      u.money -= 60000
-      u.save
-    end
-  end
-  
+    
 #  define_command 'dectest' do
 #    output "\033(0 k l m n o p q r s t u v w x }\033(B"
 #  end
