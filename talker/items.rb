07a16bd7ce2c51cca4a6e9ed33c758dde9bbe23a
TheBear <james@forluna.co.uk>
2010-03-28 17:23:44 +0100

    Price fixing

--- talker/items.rb
+++ talker/items.rb
@@ -39,9 +39,9 @@ class Items < Array
     'minibus' => Item.new("Minibus", "Salvation Army Minibus. Speed 2, Traction 1", 1, 5000),
     'lada'    => Item.new("Lada", "Lada VTFS Rally car. Speed 2, Traction 3", 1, 20000),
     'subaru'  => Item.new("Subaru", "Subaru Impreza Rally car. Speed 3, Traction 4", 1, 40000),
-    'skoda'   => Item.new("Skoda", "Skoda Fabia Rally car. Speed 4, Traction 3", 1, 60000),
-    'ford'    => Item.new("Ford", "Ford Focus RS Rally car. Speed 4, Traction 5", 1, 80000),
-    'citroen' => Item.new("Citroen", "Citroen C4 Rally car. Speed 5, Traction 4", 1, 100000)
+    'skoda'   => Item.new("Skoda", "Skoda Fabia Rally car. Speed 4, Traction 3", 1, 70000),
+    'ford'    => Item.new("Ford", "Ford Focus RS Rally car. Speed 4, Traction 5", 1, 90000),
+    'citroen' => Item.new("Citroen", "Citroen C4 Rally car. Speed 5, Traction 4", 1, 125000)
   }
 
   def add(item)
