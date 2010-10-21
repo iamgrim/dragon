d153c08fdbe1ea2ddf70b1116cc79db60e0d3473
TheBear <james@forluna.co.uk>
2010-10-21 22:24:31 +0100

    Fix spuds

--- talker/commands/cmd_system.rb
+++ talker/commands/cmd_system.rb
@@ -214,7 +214,7 @@ module Talker
     spuds = ['King Edward', 'Duke of York', 'Jersey Royal', 'Maris Piper', 'Russet Burbank', 'Yukon Gold', 'Desiree', 'Charlotte', 'Rooster', 'Golden Wonder']
     result = all_users.values.sort{|u,u2|u2.total_login_time <=> u.total_login_time}.slice(0, 15)
     count = 0
-    output box("Top Spuds On The Realm", result.map {|u| count += 1; "#{u == self ? '^L' : ''}#{sprintf("%2.d", count)}. #{sprintf("%-15.15s", spuds[count - 1] || 'Smash')} #{sprintf("%-15.15s", u.name)} #{short_time(u.total_login_time).gsub('d', ' potatoes, ').gsub('h', ' peelings')}"}.join("^n\n"))
+    output box("Top Potato Growers On The Realm", result.map {|u| count += 1; "#{u == self ? '^L' : ''}#{sprintf("%2.d", count)}. #{sprintf("%-15.15s", spuds[count - 1] || 'Smash')} #{sprintf("%-15.15s", u.name)} #{sprintf("%26.26s", short_time(u.total_login_time).gsub('d', ' potatoes, ').gsub('h', ' peelings'))}"}.join("^n\n"))
   end
   define_alias 'spuds', 'spods'
   
